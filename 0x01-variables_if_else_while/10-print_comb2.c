149a124be601e6d89da80ef3b0019c7cb1f676bf
me-kachouri <me.kachouri@gmail.com>
2020-02-06 20:35:06 +0100

    Add another file

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Description: print numbers from 00 to 99 sperated by ", " using putchar
+ * Return: void
+ */
+int main(void)
+{
+int i;
+for (i = 0; i <= 99; i++)
+{
+putchar((i/10)+'0');
+putchar((i%10)+'0');
+if (i < 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
