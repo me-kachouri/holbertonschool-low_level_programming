e00e0177580c37eaf1cdbdbd40d3f7536d4233c4
me-kachouri <me.kachouri@gmail.com>
2020-02-12 21:14:14 +0100

    Fix file content

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * main - print sum of all even fibonacci numbers under 4000000
+ * main - print the sum of all even fibonacci numbers under 4000000
  * Return: 0
  */
 
@@ -13,10 +13,10 @@ unsigned long a = 1, b = 2, s = 0, x = 2;
 while (s <= 4000000)
 {
 s = a + b;
-if (s % 2 == 0)
-x += s;
 a = b;
 b = s;
+if (s % 2 == 0)
+x += s;
 }
 printf("%li\n", x);
 return (0);
