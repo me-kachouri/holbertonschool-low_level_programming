5d382543a2898262ea95a923ef8a78cc510474af
me-kachouri <me.kachouri@gmail.com>
2020-02-12 21:49:09 +0100

    Fix file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,7 +8,7 @@
 
 void print_times_table(int n)
 {
-if (n <= 15 && n >= 0)
+if (n < 15 && n >= 0)
 {
 int i, j;
 for (i = 0; i <= n; i++)
@@ -29,7 +29,7 @@ else if (i * j > 99)
 _putchar(',');
 _putchar(' ');
 _putchar((i * j) / 100 + '0');
-_putchar(((i * j) / 10) / 10 + '0');
+_putchar(((i * j) % 100) / 10 + '0');
 _putchar((i * j) % 10 + '0');
 }
 else
