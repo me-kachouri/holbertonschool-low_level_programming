cc6cd0085cd41072e8af2c4001ba4fc078cd8979
me-kachouri <me.kachouri@gmail.com>
2020-02-28 12:56:21 +0100

    Add another file

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,7 +8,16 @@
 
 char *leet(char *s)
 {
+char ch1[] = {'a', 'e', 'o', 'l', 't'};
+char ch2[] = {'4', '3', '0', '1', '7'};
 int i, j;
-
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; ch1[j] != '\0' && ch2[j] != '\0'; j++)
+{
+if (s[i] == ch1[j] || s[i] == (ch1[j] - 32))
+s[i] = ch2[j];
+}
+}
 return (s);
 }
